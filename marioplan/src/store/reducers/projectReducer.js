0c0816e3c00f1499c8f49fae5c5472a370c83008
wijdan1995 <wijdan.w.k@outlook.sa>
2019-09-22 02:02:45 +0300

    Adding Dummy Data

--- marioplan/src/store/reducers/projectReducer.js
+++ marioplan/src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projects: [
+        { id: '1', title: 'title one is here', content: 'blah blah blaaaaah' },
+        { id: '2', title: 'title two is here', content: 'blah blah blaaaaah' },
+        { id: '3', title: 'title three is here', content: 'blah blah blaaaaah' }
+    ]
+}
 
 const projectReducer = (state = initState, action) => {
     return state
